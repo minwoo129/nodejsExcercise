60733e1bd0113b0a16e0bd9a492993b7db6176ac
minwoo129 <key0129mw@naver.com>
2022-02-06 20:19:36 +0900

    learn-mongoose: 댓글 안뜨는 오류 해결(오타 수정)

--- ch8/learn-mongoose/public/mongoose.js
+++ ch8/learn-mongoose/public/mongoose.js
@@ -1,3 +1,4 @@
+// 사용자 이름 클릭시
 document.querySelectorAll('#user-list tr').forEach(el => {
     el.addEventListener('click', function () {
         const id = el.querySelector('td').textContent;
@@ -44,72 +45,59 @@ async function getUser() {
 
 async function getComment(id) {
     try {
-        const res = await axios.get(`/users/${id}/comments`);
-        const comments = res.data;
-        console.log('comments: ', comments);
-
-        const tbody = document.querySelector('#comment-list tbody');
-        tbody.innerHTML = '';
-
-        comments.map((comment) => {
-            const row = document.createElement('tr');
-            
-            let td = document.createElement('td');
-            td.textContent = comment._id;
-            row.appendChild(td);
-
-            td = document.createElement('td');
-            td.textContent = comment.commentor.name;
-            row.appendChild(td);
-
-            td = document.createElement('td');
-            td.textContent = comment.comment;
-            row.appendChild(td);
-
-            // 수정버튼
-            const edit = document.createElement('button');
-            edit.textContent = '수정';
-            edit.addEventListener('click', async () => {
-                const newComment = prompt('바꿀 내용을 입력하세요.');
-                if(!newComment) return alert('내용을 반드시 입력하셔야 합니다.');
-
-                try {
-                    await axios.patch(`/comments/${comment._id}`, {comment: newComment});
-                    getComment(id);
-                }
-                catch(e1) {
-                    console.log('edit error: ', e1);
-                }
-            });
-
-            // 삭제버트
-            const remove = document.createElement('button');
-            remove.textContent = '삭제';
-            remove.addEventListener('click', async () => {
-                try {
-                    await axios.delete(`/comments/${comment._id}`);
-                    getComment(id);
-                }
-                catch(e2) {
-                    console.log('delete error: ', e2);
-                }
-            });
-
-            td = document.createElement('td');
-            td.appendChild(edit);
-            row.appendChild(td);
-
-            td = document.createElement('td');
-            td.appendChild(remove);
-            row.appendChild(td);
-
-            tbody.appendChild(row);
-        })
+      const res = await axios.get(`/users/${id}/comments`);
+      const comments = res.data;
+      const tbody = document.querySelector('#comment-list tbody');
+      tbody.innerHTML = '';
+      comments.map(function (comment) {
+        // 로우 셀 추가
+        const row = document.createElement('tr');
+        let td = document.createElement('td');
+        td.textContent = comment._id;
+        row.appendChild(td);
+        td = document.createElement('td');
+        td.textContent = comment.commenter.name;
+        row.appendChild(td);
+        td = document.createElement('td');
+        td.textContent = comment.comment;
+        row.appendChild(td);
+        const edit = document.createElement('button');
+        edit.textContent = '수정';
+        edit.addEventListener('click', async () => { // 수정 클릭 시
+          const newComment = prompt('바꿀 내용을 입력하세요');
+          if (!newComment) {
+            return alert('내용을 반드시 입력하셔야 합니다');
+          }
+          try {
+            await axios.patch(`/comments/${comment._id}`, { comment: newComment });
+            getComment(id);
+          } catch (err) {
+            console.error(err);
+          }
+        });
+        const remove = document.createElement('button');
+        remove.textContent = '삭제';
+        remove.addEventListener('click', async () => { // 삭제 클릭 시
+          try {
+            await axios.delete(`/comments/${comment._id}`);
+            getComment(id);
+          } catch (err) {
+            console.error(err);
+          }
+        });
+        // 버튼 추가
+        td = document.createElement('td');
+        td.appendChild(edit);
+        row.appendChild(td);
+        td = document.createElement('td');
+        td.appendChild(remove);
+        row.appendChild(td);
+        tbody.appendChild(row);
+      });
+    } catch (err) {
+      console.error(err);
     }
-    catch(e) {
-        console.log('error: ', e);
-    }
-}
+  }
 
 document.getElementById('user-form').addEventListener('submit', async (e) => {
     e.preventDefault();

e2816ca94b2a4e5c1878deab8c283f75f5c797ac
minwoo129 <key0129mw@naver.com>
2022-02-06 18:07:11 +0900

    learn-mongoose: mongodb 연결하기

---
+++ ch8/learn-mongoose/schemas/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const connect = () => {
+    if(process.env.NODE_ENV != 'production') {
+        mongoose.get('debug', true);
+    }
+    mongoose.connect('mongodb://rmw:rmwkey0129@localhost:27017/admin', {
+        dbName: 'nodejs',
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    }, (error) => {
+        if(error) console.log('mongodb 연결 err: ', error);
+        else console.log('mongodb 연결 성공');
+    });
+};
+mongoose.connection.on('error', (error) => {
+    console.log('mongodb 연결 err: ', error);
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('mongodb 연결이 끊어졌습니다. 연결을 재시도 합니다.');
+    connect();
+});
+
+module.exports = connect;

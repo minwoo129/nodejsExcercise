b88687fd3e7d980ee2b81503c77245211019c020
minwoo129 <key0129mw@naver.com>
2022-02-13 21:23:46 +0900

    nodebird: 데이터베이스 설정

--- ch9/nodebird/app.js
+++ ch9/nodebird/app.js
@@ -7,15 +7,23 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const pageRouter = require('./routes/page');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
+const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
 nunjucks.configure('views', {
     express: app,
     watch: true
 });
+sequelize.sync({ force: false })
+.then(() => {
+    console.log('데이터베이스 연결 성공');
+})
+.catch((e) => {
+    console.error(e);
+})
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));

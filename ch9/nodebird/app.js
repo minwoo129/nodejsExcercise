0bf0c05ebbe12ec2de8921494da5d440f2786151
minwoo129 <key0129mw@naver.com>
2022-02-13 20:39:55 +0900

    nodebird: 패키지 추가, app.js 추가

---
+++ ch9/nodebird/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const morgan = require('morgan');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');

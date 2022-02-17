fc871387e4ec15c2cf12bd703291a5954043fb05
minwoo129 <key0129mw@naver.com>
2022-02-17 14:18:37 +0900

    nodebird: 로컬 로그인, sns 로그인 연결

--- ch9/nodebird/app.js
+++ ch9/nodebird/app.js
@@ -5,10 +5,14 @@ const morgan = require('morgan');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
+const passportConfig = require('./passport');
+const authRouter = require('./routes/auth');
 
 dotenv.config();
 
 const app = express();
+passportConfig();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 const pageRouter = require('./routes/page');
@@ -39,7 +43,11 @@ app.use(session({
         secure: false
     }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);

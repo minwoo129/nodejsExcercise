eba12fb96f26d0ce2305f2acbdc5157152dab351
minwoo129 <key0129mw@naver.com>
2022-03-01 11:49:01 +0900

    nodebird-api, nodecat: sns api 서버 만들기

--- ch10/nodecat/app.js
+++ ch10/nodecat/app.js
@@ -12,37 +12,37 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
-    express: app,
-    watch: true
+  express: app,
+  watch: true,
 });
 
 app.use(morgan('dev'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false
-    }
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
 
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV != 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중')
-})
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
